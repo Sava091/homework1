4424598e273ea6bc6624faeea5671a764ae03373
Vitaliy <v.v.grygorovych@gmail.com>
2019-03-29 12:00:30 +0200

    First commit for homework2. added main, libs, manifest

---
+++ src/main/java/com/epam/training/javajunior/HW2/Main.java
@@ -0,0 +1,11 @@
+package com.epam.training.javajunior.HW2;
+
+import org.junit.Assert;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Vitaliy Grygorovych");
+        Assert.fail();
+    }
+}
